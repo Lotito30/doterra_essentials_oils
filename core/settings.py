b4d05bb656ca2867811cf513d251f4667504193c
lotito30 <dominguezje21@gmail.com>
2024-06-19 18:40:23 -0300

    setting

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
