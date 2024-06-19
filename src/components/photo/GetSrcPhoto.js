891b86bb3d84d1d83ca5c05336230f719b45a682
lotito30 <dominguezje21@gmail.com>
2024-06-19 18:59:36 -0300

    url

--- src/components/photo/GetSrcPhoto.js
+++ src/components/photo/GetSrcPhoto.js
@@ -1,7 +1,7 @@
 export default function GetSrcPhoto(url) {
     let srcPhoto
   if(process.env.REACT_APP_NODE_ENV === 'development'){
-    srcPhoto = `https://doterra-essentials-oils-2fbd0f7ae026.herokuapp.com/${url}`
+    srcPhoto = `http://localhost:8000/${url}`
   }else{
     srcPhoto = url
   }
