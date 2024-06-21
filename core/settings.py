f3a2d8dde788bb8730f8b5c241bb283a2f09ae87
lotito30 <dominguezje21@gmail.com>
2024-06-21 20:43:53 -0300

    u

--- core/settings.py
+++ core/settings.py
@@ -284,8 +284,8 @@ if not DEBUG:
 
     DEFAULT_FROM_EMAIL='dominguezjesu26 - <dominguezjesu26@gmail.com>'
     EMAIL_BACKEND='django.core.email.backends.smtp.EmailBackend'
-    EMAIL_HOST=env('EMAIL_HOST')
-    EMAIL_HOST_USER=env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT=env('EMAIL_PORT')
+    EMAIL_HOST=os.environ.get('EMAIL_HOST')
+    EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT=os.environ.get('EMAIL_PORT')
     EMAIL_USER_TLS=True
